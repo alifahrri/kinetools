a6af0c89dfda2b537da7cabf8a7740855856f432
Fahri Ali Rahman <fahri@nodeflux.io>
2019-11-13 20:24:28 +0700

    Initial commit

---
+++ examples/src/eigen_array.cpp
@@ -0,0 +1,71 @@
+# include <cppad/cppad.hpp>
+# include <cppad/example/cppad_eigen.hpp>
+# include <cppad/speed/det_by_minor.hpp>
+# include <Eigen/Dense>
+
+#include <iostream>
+
+bool eigen_array(void)
+{     bool ok = true;
+     using CppAD::AD;
+     using CppAD::NearEqual;
+     using Eigen::Matrix;
+     using Eigen::Dynamic;
+     //
+     typedef Matrix< AD<double> , Dynamic, 1 > a_vector;
+     //
+     // some temporary indices
+     size_t i, j;
+
+     // domain and range space vectors
+     size_t n  = 10, m = n;
+     a_vector a_x(n), a_y(m);
+
+     // set and declare independent variables and start tape recording
+     for(j = 0; j < n; j++)
+          a_x[j] = double(1 + j);
+     CppAD::Independent(a_x);
+
+     // evaluate a component wise function
+     a_y = a_x.array() + a_x.array().sin();
+
+     // create f: x -> y and stop tape recording
+     CppAD::ADFun<double> f(a_x, a_y);
+
+     std::cout << "a_x :" << std::endl;
+     std::cout << a_x << std::endl;
+     std::cout << "a_y :" << std::endl;
+     std::cout << a_y << std::endl;
+
+     // compute the derivative of y w.r.t x using CppAD
+     CPPAD_TESTVECTOR(double) x(n);
+     for(j = 0; j < n; j++)
+          x[j] = double(j) + 1.0 / double(j+1);
+     CPPAD_TESTVECTOR(double) jac = f.Jacobian(x);
+
+     // check Jacobian
+     double eps = 100. * CppAD::numeric_limits<double>::epsilon();
+     for(i = 0; i < m; i++)
+     {     for(j = 0; j < n; j++)
+          {     double check = 1.0 + cos(x[i]);
+               if( i != j )
+                    check = 0.0;
+               ok &= NearEqual(jac[i * n + j], check, eps, eps);
+          }
+     }
+
+     std::cout << "jacobian :" << std::endl;
+     for (size_t i=0; i<10; i++) {
+         std::cout << std::endl;
+         for (size_t j=0; j<10; j++) 
+            std::cout << jac[j*n+i] << ", ";
+     }
+     std::cout << std::endl;
+
+     return ok;
+}
+
+int main()
+{
+    return eigen_array();
+}
